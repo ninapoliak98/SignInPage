82bf0cc6cbfbd0070835d4e89f17d9dbcfc92a49
Nina Poliak-Karatkevich <ninapoliakkarat@gmail.com>
2022-12-23 03:05:20 -0500

    sign-up styled, added some functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Signup from "./Components/Signup";
+import {AuthProvider} from "./Context/AuthContext";
 
 function App() {
   return (
       <div>
-        <Signup/>
+         <AuthProvider>
+             <Signup/>
+         </AuthProvider>
       </div>
   );
 }
